d783ed27ea095bbb9876c1e34b7fce8046d22848
gr.torgan <60741700+gr-torgan@users.noreply.github.com>
2022-10-21 18:38:12 +0500

    Initial commit

---
+++ 2/2/2.cpp
@@ -0,0 +1,30 @@
+﻿#include <math.h>
+#include <iostream>
+#include <conio.h>
+
+long double fact(double n)
+    {
+    if (n < 0)
+        return 0;
+    if (n == 0)
+        return 1;
+    else
+        return n * fact(n - 1);
+    }
+int main()
+{
+    
+    double  sum, an, n;
+    an = 1.0;
+    sum = 0;
+    n = 1.0;
+    while (an > 1E-4)
+    {
+        an = (10 * fact(n))/(fact(n + 1));
+        sum = sum + an;
+        
+        n = n + 1.0;
+    }
+    std::cout << "Sum of the series: " << sum;
+    _getch;
+}
